db5ccb55fd04cebad1ea3071651c7a69014f020f
Frederico Cotrim <frederico.cotrim@gmail.com>
2021-11-19 17:03:09 -0300

    Initial commit

---
+++ python/MyFunctions.py
@@ -0,0 +1,99 @@
+import os
+import sys
+import base64
+from Naked.toolshed.shell import execute_js, muterun_js
+
+def encode(decodedStr):
+  encodedBytes = base64.b64encode(decodedStr.encode("utf-8"))
+  encodedStr = encodedBytes.decode("utf-8")
+  return encodedStr
+
+def decode(encodedStr):
+  decodedBytes = base64.b64decode(encodedStr.encode("utf-8"))
+  decodedStr = decodedBytes.decode("utf-8")
+  return decodedStr
+
+#print(encode("abc123!?$*&()'-=@~"))
+#print(decode("YWJjMTIzIT8kKiYoKSctPUB+"))
+
+
+
+
+
+def py_node(module, arguments):
+#     print(__file__)
+
+#     print(os.path.abspath( __file__))
+#     print(os.path.realpath(__file__))
+
+#     print(os.path.dirname(os.path.abspath(__file__)))
+#     print(os.path.dirname(os.path.realpath(__file__)))
+
+  filepath = os.path.dirname(os.path.abspath(__file__))
+  filename = "index.js"
+
+  #response = muterun_js("E:/Python/Python37/nodejs/dist/" + filename, module + " " + encode(arguments))
+  response = muterun_js(os.path.join(filepath, "nodejs", "dist", filename), module + " " + encode(arguments))
+
+  if response.exitcode == 0:
+#     print(response.stdout)
+#     print(response.stdout.decode())
+#     print(response.stdout.decode("utf-8"))
+#     print(response.stdout.decode(sys.stdout.encoding))
+    output = response.stdout.decode(sys.stdout.encoding)
+    decodedOutput = decode(output)
+    return decodedOutput
+  else:
+#     sys.stderr.write(response.stderr)
+#     sys.stderr.write(response.stderr.decode())
+    raise Exception(response.stderr.decode())
+
+
+	
+
+
+#print(py_node("dist/index.js", '{"id":1,"descricao":"PESSOAL BR"}'))
+#raise Exception("Teste erro")
+
+
+
+
+	
+#https://www.base64encoder.io/python/	
+#https://www.base64decoder.io/python/
+	
+#urlsafe_b64encode
+#urlsafe_b64decode
+
+'''
+
+print("Encode")
+print("------")
+		
+data = "abc123!?$*&()'-=@~"
+encodedBytes = base64.b64encode(data.encode("utf-8"))
+
+encodedStr = str(encodedBytes, "utf-8")
+encodedStr2 = encodedBytes.decode("utf-8")
+
+print(data)
+print(encodedBytes)
+print(encodedStr)
+print(encodedStr2)
+
+print("")
+
+print("Decode")
+print("------")
+
+decodedBytes = base64.b64decode(encodedStr)
+
+decodedStr = str(decodedBytes, "utf-8")
+decodedStr2 = decodedBytes.decode("utf-8")
+
+print(encodedStr)
+print(decodedBytes)
+print(decodedStr)
+print(decodedStr2)
+
+'''
