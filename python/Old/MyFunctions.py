81dc5a1485b677d75a987ea70a112ba6dc1ad7d1
Frederico Cotrim <frederico.cotrim@gmail.com>
2023-03-08 13:54:57 -0300

    Add old version (Python-3.3 where subprocess methods are different)

---
+++ python/Old/MyFunctions.py
@@ -0,0 +1,96 @@
+import os
+import sys
+import base64
+import subprocess
+
+def encode(decodedStr):
+  encodedBytes = base64.b64encode(decodedStr.encode("utf-8"))
+  encodedStr = encodedBytes.decode("utf-8")
+  return encodedStr
+
+def decode(encodedStr):
+  decodedBytes = base64.b64decode(encodedStr.encode("utf-8"))
+  decodedStr = decodedBytes.decode("utf-8")
+  return decodedStr
+
+#print(encode("abc123!?$*&()'-=@~"))
+#print(decode("YWJjMTIzIT8kKiYoKSctPUB+"))
+
+
+
+
+
+def py_node(module, arguments):
+#     print(__file__)
+
+#     print(os.path.abspath( __file__))
+#     print(os.path.realpath(__file__))
+
+#     print(os.path.dirname(os.path.abspath(__file__)))
+#     print(os.path.dirname(os.path.realpath(__file__)))
+
+  filepath = os.path.dirname(os.path.abspath(__file__))
+  filename = "index.js"
+
+  #response = muterun_js("E:/Python/Python37/nodejs/dist/" + filename, module + " " + encode(arguments))
+  response = subprocess.check_output(
+    ["node", os.path.join(filepath, "nodejs", "dist", filename), module, encode(arguments)], 
+    stdin=subprocess.DEVNULL,    
+    stderr=subprocess.STDOUT
+  )
+
+  encodedOutput = response.decode(sys.stdout.encoding)
+  decodedOutput = decode(encodedOutput)
+  return decodedOutput
+
+
+	
+
+
+#print(py_node("httpClient", '{ "method": "get", "url": "https://brasilapi.com.br/api/cep/v2/14770000" }'))
+#raise Exception("Teste erro")
+
+
+
+
+	
+#https://www.base64encoder.io/python/	
+#https://www.base64decoder.io/python/
+	
+#urlsafe_b64encode
+#urlsafe_b64decode
+
+
+
+'''
+
+print("Encode")
+print("------")
+		
+data = "abc123!?$*&()'-=@~"
+encodedBytes = base64.b64encode(data.encode("utf-8"))
+
+encodedStr = str(encodedBytes, "utf-8")
+encodedStr2 = encodedBytes.decode("utf-8")
+
+print(data)
+print(encodedBytes)
+print(encodedStr)
+print(encodedStr2)
+
+print("")
+
+print("Decode")
+print("------")
+
+decodedBytes = base64.b64decode(encodedStr)
+
+decodedStr = str(decodedBytes, "utf-8")
+decodedStr2 = decodedBytes.decode("utf-8")
+
+print(encodedStr)
+print(decodedBytes)
+print(decodedStr)
+print(decodedStr2)
+
+'''
